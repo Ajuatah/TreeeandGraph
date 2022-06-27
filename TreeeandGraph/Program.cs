5041769805f9579f0f5d03d4dc04d78bc72c2382
TECHRITOMA Inc <ajuatah@gmail.com>
2022-06-27 12:08:51 +0100

    Add project files.

---
+++ TreeeandGraph/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeeandGraph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree BST = new Tree();
+            Console.Write("Enter number of elements you want to have in the tree: ");
+            int numberOfElementsInTree = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= numberOfElementsInTree; i++)
+            {
+                Console.Write($"Enter {i} node: ");
+                int node = int.Parse(Console.ReadLine());
+                BST.Insert(node);
+
+            }
+
+            Console.WriteLine("Inorder Traversal is : ");
+            BST.InOrder(BST.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+
+            Console.WriteLine("pre-order Traversal is : ");
+            BST.Pre_order(BST.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+
+            Console.WriteLine("post-order Traversal is : ");
+            BST.PostOrder(BST.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+    class Node1
+    {
+        public int value;
+        public Node1 left;
+        public Node1 right;
+
+        public void Show()
+        {
+            Console.Write("[");
+            Console.Write(value);
+            Console.Write("]");
+        }
+    }
+
+    class Tree
+    {
+        public Node1 root;
+        public Tree()
+        {
+            root = null;
+        }
+
+        public Node1 ReturnRoot()
+        {
+            return root;
+        }
+
+        public void Insert(int x)
+        {
+            Node1 newNode = new Node1();
+            newNode.value = x;
+
+            if(root == null)root = newNode;
+            else
+            {
+                Node1 current = root;
+                Node1 parent;
+
+                while (true)
+                {
+                   parent = current;
+                    if (x < current.value)
+                    {
+                        current = current.left;
+                        if(current == null)
+                        {
+                            parent.left = newNode;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if(current == null)
+                        {
+                            parent.right = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        public void Pre_order(Node1 Root)
+        {
+            if(Root != null)
+            {
+                Console.Write(Root.value+" ");
+                Pre_order(Root.left);
+                Pre_order(Root.right);
+            }
+        }
+
+        public void InOrder(Node1 Root)
+        {
+            if(Root != null)
+            {
+                InOrder(Root.left);
+                Console.Write(Root.value + " ");
+                InOrder(Root.right);
+            }
+        }
+
+        public void PostOrder(Node1 Root)
+        {
+            if(Root != null)
+            {
+                PostOrder(Root.left);
+                PostOrder(Root.right);
+                Console.Write(Root.value + " ");
+            }
+        }
+    }
+        
+
+}
---
+++ TreeeandGraph/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TreeeandGraph")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TreeeandGraph")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f93e1f3a-5edf-4726-9c88-45a9342df162")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
